099b8d187aa6ecff17cfa48f35e9a43643f4bfbe
Billsen <76168595+Billsen210@users.noreply.github.com>
2024-01-19 16:53:11 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,3 +5,5 @@ data = data[data['age'] >= 1]
 data = data[data['gender'] != 'Other']
 data['gender'].replace({'Female': 0, 'Male': 1}, inplace=True)
 data_encoded = pd.get_dummies(data, columns=['smoking_history'], prefix='smoking')
+
+data_encoded.to_csv('/opt/data/df.csv', index=False)
